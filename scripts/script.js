fb74ceca1a0ac50e6a60db19160224eed1a59758
suntzar <cartundomino@gmail.com>
2024-10-19 11:51:18 -0300

    math

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,4 @@
 const galeryid = document.getElementById("galery");
-const search = document.getElementById("search");
 const editor = document.getElementById("markdown");
 const preview = document.getElementById("preview");
 let show_edit = true;
@@ -10,7 +9,7 @@ function MKFunc(markdown) {
   markdown = markdown.replace(/××(.*?)××/gim, "<center>$1</center>");
 
   // Pie
-  markdown = markdown.replace(/%(\d+)%/gim, '<div id="pie" class="pie" style="--p:$1;"><p style="color: var(--color-p); z-index:1; -webkit-text-stroke-color: var(--color-y); -webkit-text-stroke-width: 1.5px;">$1%</p></div>');
+  markdown = markdown.replace(/%(\d+)%/gim, '<div id="pie" class="pie" style="--p:$1;"><p style="color: var(--color-p); -webkit-text-stroke-color: var(--color-y); -webkit-text-stroke-width: 1.5px;">$1%</p></div>');
 
   return markdown.trim();
 }
@@ -18,6 +17,7 @@ function MKFunc(markdown) {
 editor.addEventListener("input", () => {
   const markdownText = editor.value;
   preview.innerHTML = marked.parse(MKFunc(markdownText));
+  renderMathInElement(preview); // Renderiza as expressões matemáticas
 });
 
 function edit() {
@@ -30,7 +30,7 @@ function edit() {
     editor.style.display = "none"; // Oculta o editor
   }
   if (show_galery) {
-    galeryid.style.display = "none"; // Oculta o galeria
+    galeryid.style.display = "none"; // Oculta a galeria
     editor.style.display = "none"; // Oculta o editor
     preview.style.display = "block"; // Mostra o preview
     show_galery = false;
@@ -46,7 +46,7 @@ function galery() {
     preview.style.display = "none"; // Oculta o preview
     show_edit = false;
   } else {
-    galeryid.style.display = "none"; // Oculta o galeria
+    galeryid.style.display = "none"; // Oculta a galeria
     editor.style.display = "none"; // Oculta o editor
     preview.style.display = "block"; // Mostra o preview
     show_edit = false;
@@ -56,8 +56,9 @@ function galery() {
 function updatePreview() {
   const markdownText = editor.value;
   preview.innerHTML = marked.parse(MKFunc(markdownText));
+  renderMathInElement(preview); // Renderiza as expressões matemáticas
   document.getElementById("card").innerHTML = marked.parse(MKFunc(markdownText));
-  console.log(marked.parse(MKFunc(markdownText)));
+  console.log(preview.innerHTML);
   hljs.highlightAll();
 }
 
